9538e80f8bf38b7877ed5d05f0f165769f034f77
Shoma Okamoto <shoma.develop@gmail.com>
2020-04-05 13:38:27 +0900

    Fix the title.

--- components/cards/TestedNumberCard.vue
+++ components/cards/TestedNumberCard.vue
@@ -1,7 +1,7 @@
 <template>
   <v-col cols="12" md="6" class="DataCard">
     <time-bar-chart
-      :title="$t('検査実施数 (3月20日以降)')"
+      :title="$t('検査実施数')"
       :title-id="'number-of-tested-cases'"
       :chart-id="'time-bar-chart-tested'"
       :chart-data="inspectionsGraph"
